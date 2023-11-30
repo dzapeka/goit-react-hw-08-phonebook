71a77d023bc98f1a5868fc3708f90715f971496d
dzapeka <douson@gmail.com>
2023-11-30 23:53:21 +0100

    misc fix

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,7 @@
 import LoginForm from 'components/LoginForm/LoginForm';
-import { Notify } from 'notiflix';
-import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { useSelector } from 'react-redux';
-import { selectAuthError } from 'redux/auth/auth.selectors';
 
 const LoginPage = () => {
-  const error = useSelector(selectAuthError);
-
-  useEffect(() => {
-    if (error) {
-      Notify.failure(error);
-    }
-  }, [error]);
-
   return (
     <>
       <Helmet>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,19 +1,7 @@
 import RegisterForm from 'components/RegisterForm/RegisterForm';
-import { Notify } from 'notiflix';
-import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { useSelector } from 'react-redux';
-import { selectAuthError } from 'redux/auth/auth.selectors';
 
 const RegisterPage = () => {
-  const error = useSelector(selectAuthError);
-
-  useEffect(() => {
-    if (error) {
-      Notify.failure(error);
-    }
-  }, [error]);
-
   return (
     <>
       <Helmet>
