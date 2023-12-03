9af46534eecb379290403dcc9268b611655a0674
dzapeka <douson@gmail.com>
2023-12-03 03:30:43 +0100

    fix private route

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,10 +1,19 @@
+import Loader from 'components/Loader/Loader';
 import { LOGIN_ROUTE } from 'constants/routes';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { selectIsLoggedIn } from 'redux/auth/auth.selectors';
+import {
+  selectAuthIsLoading,
+  selectIsLoggedIn,
+} from 'redux/auth/auth.selectors';
 
 const PrivateRoute = ({ children, navigateTo = LOGIN_ROUTE }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isAuthLoading = useSelector(selectAuthIsLoading);
+
+  if (isAuthLoading) {
+    return <Loader />;
+  }
 
   return isLoggedIn ? children : <Navigate to={navigateTo} replace />;
 };
